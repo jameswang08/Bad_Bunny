23704785e8a19fa7548ca0bf64a4ce0ae261e5fd
James Wang <114769089+jameswang08@users.noreply.github.com>
2022-11-25 20:56:20 -0800

    Initial Commit

---
+++ Proj7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Proj7
+//
+//  Created by jam on 11/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
